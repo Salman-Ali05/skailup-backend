80904be1da2a3388ab7beae0d90ef98f213ff689
Ali <sallukhan0805@gmail.com>
2026-01-28 15:13:40 +0100

    cors for front url

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 
 const userRoutes = require('./routes/users.routes')
 const authRoutes = require('./routes/auth.routes')
@@ -9,6 +10,14 @@ const { supabaseAnon } = require('./db/supabase')
 const app = express()
 const port = process.env.PORT || 3000
 
+// CORS configuration for specific app
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 
 // injecte le client "public" par défaut
