099227ab70da94b249168168f67eebb291bc23e0
Ali <sallukhan0805@gmail.com>
2026-05-06 14:34:09 +0200

    auth on route to get access to req.user

--- routes/contributors.routes.js
+++ routes/contributors.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const contributors_contoller = require('../controllers/contributors.controllers')
+const { authenticated } = require('../middlewares/authenticated.middleware')
 
-router.get('/', contributors_contoller.getContributors);
+router.get('/', authenticated, contributors_contoller.getContributors);
 module.exports = router
